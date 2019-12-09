4ffd414849a70fe1e855883dd2ade9ff898416ff
Neel <Neel.Mahay@nationwide.co.uk>
2019-12-09 15:29:39 +0000

    Ordering added

--- src/ReportBackend.js
+++ src/ReportBackend.js
@@ -1,10 +1,18 @@
 console.log('running');
 
+/**
+ * Importing required modules.
+ * Connecting to a config.json file which has connections and passwords for databases
+ * dependent on the profile selected.
+ */
 let express = require('express');
 let bodyparser = require('body-parser');
 let mongodb = require('mongodb')
 let mongo = require('mongodb').MongoClient;
 
+/**
+ * This is where to select the database that is referenced throughout the backend.
+ */
 let allConfig = require('./config.json');
 let profile = allConfig.currentProfile;
 let config = allConfig[profile];
@@ -21,6 +29,9 @@ app.use(bodyparser.urlencoded({ extended: true }));
 let dbName = database.name;
 let dbCollection = database.collection;
 
+/**
+ * Get request to return all reports.
+ */
 app.get('/API/getReportList', function (req, res) {
     mongo.connect(url, function (err, client) {
         if (err) throw err;
@@ -33,6 +44,29 @@ app.get('/API/getReportList', function (req, res) {
     });
 });
 
+/**
+ * Get request to return all reports that can be ordered by ascending or descending date.
+ */
+app.get('/API/getReportListByDate/:order', function (req, res) {
+    if (req.params.order === "asc") {
+        var mysort = { date_created: 1 };
+    } else if(req.params.order === "desc"){
+        var mysort = { date_created: -1 };
+    }
+    mongo.connect(url, function (err, client) {
+        if (err) throw err;
+        db = client.db(dbName);
+        db.collection(dbCollection).find().sort(mysort).toArray(function (err, result) {
+            if (err) throw err;
+            res.send(result);
+        });
+        client.close();
+    });
+});
+
+/**
+ * Get request to return all reports that belong to a single user.
+ */
 app.get('/API/getReportById/:id', function (req, res) {
     let data = {
         _id: new mongodb.ObjectID(req.params.id)
@@ -48,24 +82,30 @@ app.get('/API/getReportById/:id', function (req, res) {
     });
 });
 
+/**
+ * Post request to create report. Expects JSON for data.
+ */
 app.post('/API/postReport', function (req, res) {
     data = req.body;
     mongo.connect(url, function (err, client) {
         if (err) {
-            console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+            console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
         } else {
             db = client.db(dbName)
             db.collection(dbCollection).insertOne(data, function (error, result) {
                 if (error) {
-                    console.log('Error occurred while inserting record...\n',error);
+                    console.log('Error occurred while inserting record...\n', error);
                 }
             });
             console.log("success");
-            
+
             res.send("Success");
             client.close();
         }
     });
 });
 
+/**
+ * Port number read from config.json file.
+ */
 app.listen(config.node_port_report_backend);
